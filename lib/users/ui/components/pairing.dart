7d1d81323513def912735b4243552ea09421d5f1
marin <marin.glavas@outlook.com>
2023-01-21 16:28:38 +0100

    fix pairing keyboard type

--- lib/users/ui/components/pairing.dart
+++ lib/users/ui/components/pairing.dart
@@ -145,6 +145,7 @@ class PairWithStep extends Step {
           content: TextFormField(
             onChanged: context.read<PairEditCubit>().onPairChange,
             initialValue: context.read<PairEditCubit>().state.pair,
+            keyboardType: TextInputType.number,
           ),
           title: const Text('Pair with'),
           subtitle: const _Pair(),
